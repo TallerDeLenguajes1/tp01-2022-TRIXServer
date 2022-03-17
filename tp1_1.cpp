954c5a46318816eb563863394d8b6fa24758a72b
TRIXServer.com <admin@trixserver.com>
2022-03-17 19:43:11 -0300

    commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,7 +1,16 @@
 #include <stdio.h>
 
+int num;
+int *pnum;
+
 int main(int argc, char const *argv[])
 {
+    num = 56;
+    pnum = num;
+
+    printf("&f", pnum);
+
     printf("Hola Mundo");
+
     return 0;
 }
