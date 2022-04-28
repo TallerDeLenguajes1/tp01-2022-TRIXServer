663e2dd49cce32e94375d755c3dcba9989a00236
TRIXServer <admin@trixserver.com>
2022-04-28 18:21:08 -0300

    segundo commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,16 +1,24 @@
 #include <stdio.h>
 
 int num;
-int *pnum;
+int * pnum;
 
 int main(int argc, char const *argv[])
 {
     num = 56;
     pnum = &num;
 
-    printf("&f", pnum);
+    printf("Hola Mundo\n");
 
-    printf("Hola Mundo ");
+    printf("El contenido del puntero %p\n", pnum);
 
+    printf("La dirección de memoria almacenada por el puntero %p\n", &num);
+
+    printf("la dirección de memoria de la variable %p\n", &num);
+
+    printf("la dirección de memoria del puntero %p\n", &pnum);
+
+    printf("el tamaño de memoria utilizado por esa variable %lu\n", sizeof(num));
+    
     return 0;
 }
